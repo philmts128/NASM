5e524cf8ff3a860bf7786bd77cad5d58bc0aa120
Philippe Matias <96602120+philmts128@users.noreply.github.com>
2025-05-31 16:01:36 -0300

    Update 001_hello.asm

--- Tutorials_Point/001_hello.asm
+++ Tutorials_Point/001_hello.asm
@@ -1,19 +1,21 @@
-; hello world em Assembly x86 com NASM em ambiente Unix
-; Autor: Philipe Matias
-
-section .text
-    global _start ; identifica seção de código para o linker
+; -----------------------------------
+; Hello world em assembly x64 com NASM em Unix
+; Autor: Philippe Matias
+; -----------------------------------
 
 section .data
-    msg db 'Game Over!', 0xb  ; define string
-    len equ $ - msg           ; define tamanho da string
+    msg db 'Game Over!', 10 ;nossa mensagem a ser impressa
 
-_start:           ; esse é o ponto de entrada do programa
-    mov edx, len  ; move tamanho da string para edx
-    mov ecx, msg  ; move ponteiro para string para registrador ecx
-    mov ebx, 1    ; descritor de saída do unix (stdout)
-    mov eax, 4    ; chamada de sistema para saída de dados (sys_write)
-    int 0x80      ; interrupção para entrar em modo kernel
+section .text
+    global _start ;ponto de entrada do programa
 
-    mov eax, 1 ; chamada de sistema para encerrar programa
-    int 0x80
+_start:
+    mov rax, 1      ; chamada de sistem para escrita da mensagem (sys_write) 
+    mov rdi, 1      ; descritor da chamada de sistema, que para output é 1 
+    mov rsi, msg    ; endereço da string 
+    mov rdx, 10     ; tamanho da string: 10 bytes
+    syscall         ; realiza chamada de sistema chamando o kernel
+
+    mov rax, 60     ; encerra o progrma
+    xor rdi, rdi
+    syscall
